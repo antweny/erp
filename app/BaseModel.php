49ad5a7e0696331a72ac160725301a5a9eca4d76
antweny <antweny@live.com>
2020-01-20 12:38:14 +0300

    update the base model file base model file by adding the default Uuid trait which will change all the model id from autoincrement to uuid

--- app/BaseModel.php
+++ app/BaseModel.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\Uuids;
 
 class BaseModel extends Model
 {
+    use Uuids;
 
+    /**
+     * The attributes that should be cast to native types.
+     */
+    protected $casts = [
+        'id' => 'string'
+    ];
 }
