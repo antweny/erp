961dc6c86e701eaf8bf8d41de8d589e6b4698874
antweny <antweny@live.com>
2020-01-20 14:08:07 +0300

    update User and Job migration files changing id attribute to uuid data type and update User Model file adding the Uuid trait and casts the id ro string

--- app/User.php
+++ app/User.php
@@ -6,11 +6,12 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-
+use App\Traits\Uuids;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use Uuids;
 
     /**
      * The attributes that are mass assignable.
@@ -37,5 +38,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'id' => 'string'
     ];
 }
