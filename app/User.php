b0f75b126dbb1abb5768311d50e016f48757856a
antweny <antweny@live.com>
2020-01-21 18:30:05 +0300

    modify user model

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 use App\Traits\Uuids;
 
@@ -12,6 +13,7 @@ class User extends Authenticatable
 {
     use Notifiable;
     use Uuids;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
