e55a239932f5bb77247097ba048085263df6a1fb
antweny <antweny@live.com>
2020-01-24 17:41:07 +0300

    Roles and Permission HTTP request validation files

---
+++ app/Http/Requests/RoleRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RoleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        switch ($this->method())
+        {
+
+            case 'GET':
+            case 'DELETE': {
+                return [];
+            }
+
+            case 'POST': {
+                return [
+                    'name' => 'required|string|max:255|unique:roles,name',
+                    'guard_name' => 'required|string',
+                    'roles' => 'nullable',
+                    'desc' => 'string|nullable',
+                ];
+            }
+
+            case 'PUT':
+            case 'PATCH': {
+                return [
+                    'name' => 'required|string|max:255|unique:roles,id,'.$this->role->id,
+                    'guard_name' => 'required|string',
+                    'roles' => 'nullable',
+                    'desc' => 'string|nullable',
+                ];
+            }
+        }
+    }
+}
