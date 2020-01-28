de2d3d37aba7b06c153cdb9fc67b87c10adbdd1d
antweny <antweny@live.com>
2020-01-28 13:49:21 +0300

    Add Department form validation request file

---
+++ app/Http/Requests/DepartmentRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DepartmentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        switch ($this->method())
+        {
+
+            case 'GET':
+            case 'DELETE': {
+                return [];
+            }
+
+            case 'POST': {
+                return [
+                    'name' => 'required|string|max:255|unique:departments,name',
+                    'desc' => 'string|nullable',
+                    'sort' => 'integer|nullable',
+                ];
+            }
+
+            case 'PUT':
+            case 'PATCH': {
+                return [
+                    'name' => 'required|string|max:255|unique:departments,id,'.$this->department->id,
+                    'desc' => 'string|nullable',
+                    'sort' => 'integer|nullable',
+                ];
+            }
+        }
+    }
+}
