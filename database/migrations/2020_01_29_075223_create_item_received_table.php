8ec5772f1456a0886bac1a9024dd8aa89413b042
antweny <antweny@live.com>
2020-01-29 17:20:56 +0300

    Create Item Received Migration

---
+++ database/migrations/2020_01_29_075223_create_item_received_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemReceivedTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_received', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->date('date_received');
+
+            $table->uuid('item_id')->nullable();
+            $table->foreign('item_id')->references('id')->on('items')->onUpdate('cascade')->onDelete('SET NULL');
+
+            $table->mediumText('desc')->nullable();
+
+
+            $table->decimal('unit_rate',13,2);
+            $table->bigInteger('quantity');
+
+            $table->decimal('amount',13,2);
+            $table->mediumText('remarks')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_received');
+    }
+}
