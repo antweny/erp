3dd0a78c9da902111661ed6d221bef484146f351
antweny <antweny@live.com>
2020-02-03 16:11:21 +0300

    modify database seeders

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Permission;
 
 class PermissionsTableSeeder extends Seeder
 {
@@ -11,6 +13,40 @@ class PermissionsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('permissions')->delete();
+
+        $permissions = [
+            ['name' => 'itemUnit-read', 'guard_name' => 'admin'],
+            ['name' => 'itemUnit-create', 'guard_name' => 'admin'],
+            ['name' => 'itemUnit-update', 'guard_name' => 'admin'],
+            ['name' => 'itemUnit-delete', 'guard_name' => 'admin'],
+            ['name' => 'itemReceived-read', 'guard_name' => 'admin'],
+            ['name' => 'itemReceived-create', 'guard_name' => 'admin'],
+            ['name' => 'itemReceived-update', 'guard_name' => 'admin'],
+            ['name' => 'itemReceived-delete', 'guard_name' => 'admin'],
+            ['name' => 'itemCategories-read', 'guard_name' => 'admin'],
+            ['name' => 'itemCategories-create', 'guard_name' => 'admin'],
+            ['name' => 'itemCategories-update', 'guard_name' => 'admin'],
+            ['name' => 'itemCategories-delete', 'guard_name' => 'admin'],
+            ['name' => 'itemIssued-read', 'guard_name' => 'admin'],
+            ['name' => 'itemIssued-create', 'guard_name' => 'admin'],
+            ['name' => 'itemIssued-update', 'guard_name' => 'admin'],
+            ['name' => 'itemIssued-delete', 'guard_name' => 'admin'],
+            ['name' => 'item-read', 'guard_name' => 'admin'],
+            ['name' => 'item-create', 'guard_name' => 'admin'],
+            ['name' => 'item-update', 'guard_name' => 'admin'],
+            ['name' => 'item-delete', 'guard_name' => 'admin'],
+            ['name' => 'department-read', 'guard_name' => 'admin'],
+            ['name' => 'department-create', 'guard_name' => 'admin'],
+            ['name' => 'department-update', 'guard_name' => 'admin'],
+            ['name' => 'department-delete', 'guard_name' => 'admin'],
+        ];
+
+        foreach($permissions as $permission){
+            Permission::create($permission);
+        }
+
+
+
     }
 }
