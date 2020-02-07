f20b6d087c7485b01173677a636daf709df81d37
antweny <antweny@live.com>
2020-02-07 11:43:20 +0300

    update the system layout by adding select2 and date picker jquery plugins

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,15 +10,27 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <!-- google font -->
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <!-- font awesome icons-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" >
+
+    <!-- -->
+    <link href="{{ asset('vendor/gijgo/gijgo.min.css') }}" rel="stylesheet">
+
     <!-- DataTables -->
     <link href="{{ asset('vendor/DataTables/dataTables.min.css') }}" rel="stylesheet">
+
+    <!-- Select Dropdown -->
+    <link href="{{ asset('vendor/select/select.min.css') }}" rel="stylesheet">
+
+
+
+
+
+
 
     <!-- custom style -->
     <link href="{{ asset('css/general.css') }}" rel="stylesheet">
@@ -40,15 +52,53 @@
 </div>
 
 <script src="{{ asset('js/app.js') }}"></script>
-
-<!-- Affix Js-->
+<script src="{{ asset('vendor/gijgo/gijgo.min.js') }}" ></script>
 <script src="{{ asset('vendor/DataTables/dataTables.min.js') }}" ></script>
+<script src="{{ asset('vendor/select/select.min.js') }}" ></script>
+
 <script>
+
     $(document).ready( function () {
         $('#table').DataTable({
             "iDisplayLength": 25
         });
+        $('#activityTable').DataTable({
+            "iDisplayLength": 25
+        });
+        // Material Multi Select Initialization
+        $('.multiple-select-id').select2({
+
+        });
+        $('b[role="presentation"]').hide()
+
+        // Material Multi Select Initialization
+        $('.single-select').select2();
+        //Count number of words in event textarea
     } );
+
+
+    $('#start_date').datepicker({
+        uiLibrary: 'bootstrap4',
+        iconsLibrary: 'fontawesome',
+        //format: 'yyyy-dd-mm'
+    });
+
+    $('#end_date').datepicker({
+        uiLibrary: 'bootstrap4',
+        iconsLibrary: 'fontawesome',
+        //format: 'yyyy-dd-mm'
+    });
+
+    $('#dob').datepicker({
+        uiLibrary: 'bootstrap4',
+        iconsLibrary: 'fontawesome',
+        //format: 'yyyy-dd-mm'
+    });
+
+    $(document).ready(function () {
+        $("#editor").editor();
+    });
+
 </script>
 
 
